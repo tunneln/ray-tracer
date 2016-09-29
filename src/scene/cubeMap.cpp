4ce200f134717ef507db8737dfe3bafd7607f599
Noel Negusse <noel.blob@gmail.com>
2016-09-29 05:04:51 -0500

    Implemented CubeMap

--- src/scene/cubeMap.cpp
+++ src/scene/cubeMap.cpp
@@ -4,7 +4,31 @@
 extern TraceUI* traceUI;
 
 glm::dvec3 CubeMap::getColor(ray r) const {
+    glm::dvec3 normals[6] = {glm::dvec3(1, 0, 0), 
+							glm::dvec3(-1, 0, 0), 
+							glm::dvec3(0, 1, 0), 
+							glm::dvec3(0, -1, 0),                    
+							glm::dvec3(0, 0, 1), 
+							glm::dvec3(0, 0, -1)};
+    int min_index = -1;
+    double min = 0;
+    for (size_t i = 0; i < 6; i++) {
+		double t = 1.0 / glm::dot(normals[i], r.d);
+        if (t > 0 && (t < min || min_index == -1)) {
+            min_index = i;
+            min = t;
+        }
+    }
 
-	// YOUR CODE HERE
-	// FIXME: Implement Cube Map here
+    glm::dvec3 inter_point = r.d * min;
+    glm::dvec3 proj = inter_point - glm::dot(inter_point, normals[min_index]) * normals[min_index];
+    glm::dvec2 proj_2d(0, 0);
+
+    if (min_index < 2) proj_2d = glm::dvec2(proj.z, proj.y);
+    if (min_index == 2 || min_index == 3) proj_2d = glm::dvec2(proj.x, proj.z);
+    if (min_index == 4 || min_index == 5) proj_2d = glm::dvec2(proj.x, proj.y);
+
+    glm::dvec2 coord = (proj_2d + glm::dvec2(1, 1)) / 2.0;
+
+	return tMap[min_index]->getMappedValue(coord);
 }
